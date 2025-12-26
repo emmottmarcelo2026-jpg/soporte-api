6d9da3c1f85199ca93723ac2a570a45b4307eb8a
emmottmarcelo2026-jpg <emmottmarcelo2026@gmail.com>
2025-12-26 11:50:46 -0300

    feat(users): define users domain model without authentication

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,51 @@
-export class Usuario {}
+import { Area } from "src/areas/entities/area.entity";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { UserStatus } from "../enums/user-status.enum";
+import { Role } from "../roles/entities/role.entity";
+
+@Entity('usuarios')
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'uuid',
+    default: () => 'gen_random_uuid()',
+    unique: true,
+  })
+  publicId: string;
+
+  @Column({ nullable: true, unique: true })
+  rut?: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true, select: false })
+  passwordHash?: string; // preparado, no usado aún
+
+  @ManyToOne(() => Role, { eager: true })
+  role: Role;
+
+  @ManyToOne(() => Area, { eager: true })
+  area: Area;
+
+  @Column({
+    type: 'enum',
+    enum: UserStatus,
+    default: UserStatus.ACTIVE,
+  })
+  status: UserStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
